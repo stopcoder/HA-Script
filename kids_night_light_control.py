48177f3f5a135140e9839a77c550a1fdce876a07
Jiawei Cao <jiawei.cao@sap.com>
2025-11-29 22:32:46 +0100

    change the time to turn off kids light

--- kids_night_light_control.py
+++ kids_night_light_control.py
@@ -3,7 +3,7 @@ def turn_on_night_lights():
     light.turn_on(entity_id="light.nachtlicht_mia_light", brightness=64, rgb_color=(255, 255, 197))
     light.turn_on(entity_id="light.nachtlicht_xiangbei_light", brightness=64, rgb_color=(255, 255, 197))
 
-@time_trigger("cron(0 23 * * *)")
+@time_trigger("cron(50 23 * * *)")
 def turn_off_night_lights():
     light.turn_off(entity_id="light.nachtlicht_mia_light")
     light.turn_off(entity_id="light.nachtlicht_xiangbei_light")
